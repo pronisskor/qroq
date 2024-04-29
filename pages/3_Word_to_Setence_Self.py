5b41ea5132878d33b72ef9f00ee649603f2357bc
pronisskor <163367282+pronisskor@users.noreply.github.com>
2024-04-29 22:31:13 +0900

    Update 3_Word_to_Setence_Self.py

--- pages/3_Word_to_Setence_Self.py
+++ pages/3_Word_to_Setence_Self.py
@@ -6,7 +6,6 @@ import os
 import pandas as pd
 import random
 from groq import Groq
-import json
 
 # Streamlit 페이지 타이틀 설정
 st.title("🦜🔗 Word to Sentence")
@@ -51,51 +50,131 @@ if uploaded_file is not None and not st.session_state['words_list']:
         st.session_state['words_list'] = df[words_column].dropna().tolist()
         random.shuffle(st.session_state['words_list'])
 
-def load_messages():
-    file_path = os.path.join(os.getcwd(), "pages", "messages.json")
-    if os.path.exists(file_path):
-        with open(file_path, 'r') as file:
-            messages = json.load(file)
-        return messages
-    else:
-        raise FileNotFoundError("messages.json 파일을 찾을 수 없습니다.")
-
 def generate_sentence_with_word(word):
-    print(f"Generating sentence for word: {word}")  # 단어 확인 로그 추가
-    messages = load_messages()
-
     try:
         client = Groq(api_key=groq_api_key)
         completion = client.chat.completions.create(
             model="llama3-8b-8192",
-            messages=messages,
-            temperature=1,
-            max_tokens=1024,
-            top_p=1,
-            stream=True,
-            stop=None,
+            messages=[
+        {
+            "role": "system",
+            "content": "You are an English tutor who generates sentences with a word when it is presented. When generating words, you should use easy and simple words that English beginners can know. You should make only 1 short sentence that you use a lot in your real life."
+        },
+        {
+            "role": "user",
+            "content": "laptop"
+        },
+        {
+            "role": "assistant",
+            "content": "I often use my laptop to check my email."
+        },
+        {
+            "role": "user",
+            "content": "coffee"
+        },
+        {
+            "role": "assistant",
+            "content": "I drink coffee every morning."
+        },
+        {
+            "role": "user",
+            "content": "TV"
+        },
+        {
+            "role": "assistant",
+            "content": "I watch TV with my family on Friday nights."
+        },
+        {
+            "role": "user",
+            "content": "fridge"
+        },
+        {
+            "role": "assistant",
+            "content": "I keep my juice in the fridge."
+        },
+        {
+            "role": "user",
+            "content": "table"
+        },
+        {
+            "role": "assistant",
+            "content": "We have dinner on the table at 6 pm."
+        },
+        {
+            "role": "user",
+            "content": "computer"
+        },
+        {
+            "role": "assistant",
+            "content": "My brother works on the computer all day."
+        },
+        {
+            "role": "user",
+            "content": "chair"
+        },
+        {
+            "role": "assistant",
+            "content": "My mom sits in the chair to read a book."
+        },
+        {
+            "role": "user",
+            "content": "easy"
+        },
+        {
+            "role": "assistant",
+            "content": "Learning English is easy for beginners."
+        },
+        {
+            "role": "user",
+            "content": "youtube"
+        },
+        {
+            "role": "assistant",
+            "content": "I often watch educational videos on YouTube."
+        },
+        {
+            "role": "user",
+            "content": "watch"
+        },
+        {
+            "role": "assistant",
+            "content": "I like to watch a movie on the weekend."
+        },
+        {
+            "role": "user",
+            "content": "apple"
+        },
+        {
+            "role": "assistant",
+            "content": "My favorite fruit is a juicy apple."
+        }
+    ],
+    temperature=1,
+    max_tokens=1024,
+    top_p=1,
+    stream=True,
+    stop=None,
         )
-        print("API response received")  # API 응답 로그
         response = completion.choices[0].message.content
-        print(f"Response content: {response}")  # 응답 내용 로그
-
+        # 응답 파싱
         lines = response.split('\n')
         english_sentence, korean_translation = None, None
         for line in lines:
             cleaned_line = line.strip().strip('"')
             if '**English:**' in cleaned_line:
                 english_sentence = cleaned_line.replace('**English:**', '').strip().strip('"')
+                english_sentence = english_sentence.replace('**', '')  # '**' 제거
             elif '**Korean:**' in cleaned_line:
                 korean_translation = cleaned_line.replace('**Korean:**', '').strip().strip('"')
+                korean_translation = korean_translation.replace('**', '')  # '**' 제거
 
         if english_sentence and korean_translation:
             return english_sentence, korean_translation
         else:
             raise ValueError("Response does not contain expected format of English and Korean sentences.")
     except Exception as e:
-        print(f"API 호출 중 오류가 발생했습니다: {e}")  # 오류 상세 로그
+        st.error(f"API 호출 중 오류가 발생했습니다: {e}")
         return None, None
-
 
 if st.session_state.get('words_list'):
     random_word = st.session_state['words_list'].pop(0)
@@ -111,6 +190,6 @@ if st.session_state.get('words_list'):
 if uploaded_file is not None and 'words_list' in st.session_state:
     if st.button("다음 단어"):
         if not st.session_state['words_list']:
-            st.markown(f'<p style="background-color: #bffff2; padding: 10px;">모든 단어에 대한 문장을 생성했습니다.</p>', unsafe_allow_html=True)
+            st.markdown(f'<p style="background-color: #bffff2; padding: 10px;">모든 단어에 대한 문장을 생성했습니다.</p>', unsafe_allow_html=True)
             del st.session_state['words_list']
             st.session_state['learned_count'] = 0  # 학습 카운터 초기화
